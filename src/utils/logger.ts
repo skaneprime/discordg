499cae07b5c0880d104ae8dfb94496e3b8f4a3be
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-02-14 22:55:29 +0300

    default logger export

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,6 @@
-export const Logger: Dingir.Logger.LoggerService = Dingir.Logger.create({
+export const logger: Dingir.Logger.LoggerService = Dingir.Logger.create({
 	label: "DISCRD",
 	// file: "./logs/discord.log"
 }).enableLevel(1);
+
+export default logger;
