51ce1288b0b21865cbd7dbea8a7a91c574a3ea2b
SkanePrime <50551032+skaneprime@users.noreply.github.com>
2022-04-11 12:47:29 +0300

    Plugins update, configs, database support

--- src/client/modules/services/plugins/types.ts
+++ src/client/modules/services/plugins/types.ts
@@ -1,26 +1,35 @@
 import { Awaitable, ClientEvents } from "discord.js";
 import { Client } from "../../..";
 import { ServiceObject as SO } from "..";
+import { Database } from "./database";
 
+/** @public */
+export { Database };
+/** @public */
 export type EventsListeners = {
-	[K in keyof ClientEvents as `on${Capitalize<K>}`]?: Dingir.utils.function.Any<
+	[K in keyof ClientEvents as `on${Capitalize<K>}`]?: Dingir.Utils.Function.Any<
 		ClientEvents[K], //[...],
 		void
 	>;
 };
-
-export type PluginParameters<T extends SO> = [
+/** @public */
+export type Config<T> = {
+	getPath(): string;
+	get(type?: "yml" | "json"): T;
+	save(data: T): Promise<void>;
+};
+/** @public */
+export type PluginParameters<T extends SO = SO> = [
 	{
 		client: Client<T>;
-		onMessage: (callback: (...args: unknown[]) => void) => void;
+		database: Database<T>;
+		onMessage: (callback: (id: string, name: string, ...args: unknown[]) => void) => void;
+		getConfig<T>(defaultData: T): Config<T>;
+		getConfig<T>(filename: string, defaultData: T): Config<T>;
 	},
 ];
 
-export type Plugin<T extends SO = SO> = Dingir.utils.function.Any<
+export type Plugin<T extends SO = SO> = Dingir.Utils.Function.Any<
 	PluginParameters<T>,
-	Awaitable<EventsListeners>
+	Awaitable<EventsListeners | void>
 >;
-
-/**
- * Database ->
- */
